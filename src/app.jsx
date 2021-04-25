09ddc83e5a280a0bdbc6f10f09d9889eae9746db
Kelly Wu <kelly.wu@edu.uwaterloo.ca>
2021-04-24 23:18:11 -0400

    nav

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Redirect } from 'react-router-dom';
-import React, { Suspense, lazy } from 'react';
-import { MuiThemeProvider } from '@material-ui/core';
-import styled from 'styled-components';
-import { LoadingIndicator } from './commons/components/loading-indicator';
-import { TraverseRoutes } from './commons/components/traverse-routes';
-import { theme } from './commons/theme';
+import { BrowserRouter, Redirect } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { MuiThemeProvider } from "@material-ui/core";
+import styled from "styled-components";
+import { LoadingIndicator } from "./commons/components/loading-indicator";
+import { TraverseRoutes } from "./commons/components/traverse-routes";
+import { theme } from "./commons/theme";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     exact: true,
     render: () => <Redirect to="/auth" />,
   },
 
   {
-    path: '/auth',
-    component: lazy(() => import('./modules/auth')),
+    path: "/auth",
+    component: lazy(() => import("./modules/auth")),
+  },
+
+  {
+    path: "/hrt",
+    component: lazy(() => import("./modules/hrt")),
   },
 ];
 
